1f204388dbb91ff4386156aa3046168817cf194a
UaHig <golosnyyigor@gmail.com>
2024-02-07 21:31:41 +0100

    feat: add json

--- react-intro-project/src/components/EffectSection.jsx
+++ react-intro-project/src/components/EffectSection.jsx
@@ -1,26 +1,51 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Button from "./Button/Button"
 import Modal from "./Modal/Modal"
+import ListItem from "./ListItem"
 
 export default function EffectSection() {
-const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [laoading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
 
-  function openModal() {
-    setModal(true)
+  async function fetchUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users = await response.json()
+    setUsers(users)
+    setLoading(false)
   }
 
+  useEffect(() => {
+    fetchUsers()
+  }, [])
 
   return (
     <section>
       <h3>Effects</h3>
 
-      <Button onClickfromAppJS={openModal}>Открыть информацию</Button>
+      <Button onClickfromAppJS={() => setModal(true)}>
+        Открыть информацию
+      </Button>
 
       <Modal open={modal}>
         <h3>Hello from modal</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolorem minus soluta quod, nisi fugit tenetur, laudantium fuga suscipit natus optio repellat possimus laborum aspernatur deserunt vel nam excepturi harum.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          dolorem minus soluta quod, nisi fugit tenetur, laudantium fuga
+          suscipit natus optio repellat possimus laborum aspernatur deserunt vel
+          nam excepturi harum.
+        </p>
         <Button onClickfromAppJS={() => setModal(false)}>Close modal</Button>
       </Modal>
+
+      {laoading && <p>Loading...</p>}
+      {!laoading && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </section>
   )
 }
--- react-intro-project/src/components/Header/header.jsx
+++ react-intro-project/src/components/Header/header.jsx
@@ -1,5 +1,5 @@
 import logo from "../../images/logo-name.svg"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { styled } from "styled-components"
 // import "./header.css"
 
@@ -15,7 +15,14 @@ const HeaderContainer = styled.header`
 
 export default function Header() {
   const [now, setNow] = useState(new Date())
-  setInterval(() => setNow(new Date()), 1000)
+  useEffect(() => {
+   const interval = setInterval(() => setNow(new Date()), 1000)
+
+   return () => {
+    clearInterval(interval)
+   }
+  }, [])
+
   return (
     <HeaderContainer>
       <img src={logo} alt='Result' />
